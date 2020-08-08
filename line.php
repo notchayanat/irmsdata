5713411ec16f8c3c2eb02348aed505999d9a9b3b
notchayanat <68417928+notchayanat@users.noreply.github.com>
2020-08-08 22:47:33 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'MwQZRlk77bxy84pgoGdz9PBXVfx8vokNhAPQFHkpZTWX9lTBDVhPKVWTkdra5POifsskQtHiiCNOCU3yuaIsv6Pm0ABLs8SOFxbiPEs8OpvUoCxl2n1qxnK/DOMZ/tFTezTmdCDNN883XRCMiy/K/gdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
