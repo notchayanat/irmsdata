fba6c8ecd7f4219cadba1efda508f9e2dea2a3d4
notchayanat <68417928+notchayanat@users.noreply.github.com>
2020-08-08 22:54:43 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'knight12345507',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
